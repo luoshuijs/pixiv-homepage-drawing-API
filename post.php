4af00c393f799facaf3c4f8b50a241e9ee713de2
洛水.山岭居室 <46256932+luoshuijs@users.noreply.github.com>
2019-08-08 18:24:04 +0800

    Update post.php

--- post.php
+++ post.php
@@ -1,7 +1,7 @@
 <?php
 
 
-function request_get($url, $referer){
+function request_get($url, $refere=''){
     $curl = curl_init();
     curl_setopt($curl, CURLOPT_URL, $url);
     curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36');
